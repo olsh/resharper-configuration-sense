7c081dd89bc444ff161680bf2450ccd6796e294d
Oleg Shevchenko <olsh.me@gmail.com>
2016-09-06 02:49:11 +0300

    Add build.cmd.

---
+++ build.cmd
@@ -0,0 +1,16 @@
+@echo off
+set config=%1
+if "%config%" == "" (
+   set config=Release
+)
+
+set version=2016.2.1
+if not "%PackageVersion%" == "" (
+   set version=%PackageVersion%
+)
+
+nuget restore src\Resharper.ConfigurationSense.sln
+
+"%ProgramFiles(x86)%\MSBuild\14.0\Bin\msbuild" src\Resharper.ConfigurationSense.sln /t:Rebuild /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+
+nuget pack resharper.nuspec -NoPackageAnalysis -Version %version% -Properties "Configuration=%config%;ReSharperDep=Wave;ReSharperVer=[6.0]"
